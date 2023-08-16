9315526af3a4831370b9455ab43d96c08abf0422
AdityaAndaluri <cs21b002@iittp.ac.in>
2023-08-16 20:06:02 +0530

    UIchangestoregpage

--- lib/screens/auths/register_screen.dart
+++ lib/screens/auths/register_screen.dart
@@ -12,26 +12,96 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  bool _isPasswordVisible = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/login_bg.png'),
-            fit: BoxFit.none,
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildAppLogo(),
+                _buildHeading(),
+                SizedBox(height: 16),
+                _buildDescription(), // Added description
+                SizedBox(height: 16),
+                _buildRegisterForm(),
+              ],
+            ),
           ),
         ),
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  _buildRegisterForm(),
-                ],
+      ),
+    );
+  }
+
+  Widget _buildAppLogo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Icon(
+          Icons.nature,
+          color: Color.fromARGB(255, 91, 142, 85),
+          size: 48,
+        ),
+        SizedBox(width: 8),
+        Text(
+          'GardenCompanion',
+          style: TextStyle(
+            fontFamily: 'Sf',
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildHeading() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16),
+      child: RichText(
+        textAlign: TextAlign.left,
+        text: TextSpan(
+          text: 'Register on ',
+          style: TextStyle(
+            fontFamily: 'Sf',
+            fontSize: 18,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+          children: [
+            TextSpan(
+              text: 'GardenCompanion',
+              style: TextStyle(
+                fontFamily: 'Sf',
+                fontSize: 18,
+                color: Color.fromARGB(255, 91, 142, 85),
+                fontWeight: FontWeight.bold,
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDescription() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16),
+      child: RichText(
+        textAlign: TextAlign.left,
+        text: TextSpan(
+          text: 'Create an account. We can\'t wait to have you',
+          style: TextStyle(
+            fontFamily: 'Sf',
+            fontSize: 16,
+            color: Colors.black,
           ),
         ),
       ),
@@ -42,9 +112,12 @@ class _RegisterPageState extends State<RegisterPage> {
     return Form(
       key: _formKey,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           _buildEmailTextField(),
+          SizedBox(height: 16),
           _buildPasswordTextField(),
+          SizedBox(height: 32),
           _buildRegisterButton(),
         ],
       ),
@@ -55,7 +128,12 @@ class _RegisterPageState extends State<RegisterPage> {
     return TextFormField(
       controller: _emailController,
       keyboardType: TextInputType.emailAddress,
-      decoration: const InputDecoration(labelText: 'Email'),
+      decoration: InputDecoration(
+        labelText: 'Email',
+        filled: true,
+        fillColor: Color.fromARGB(255, 247, 250, 244),
+        prefixIcon: Icon(Icons.email, color: Color.fromARGB(255, 91, 142, 85)),
+      ),
       validator: (value) {
         if (value == null || value.isEmpty) {
           return 'Please enter your email';
@@ -68,8 +146,24 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget _buildPasswordTextField() {
     return TextFormField(
       controller: _passwordController,
-      obscureText: true,
-      decoration: const InputDecoration(labelText: 'Password'),
+      obscureText: !_isPasswordVisible,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        filled: true,
+        fillColor: Color.fromARGB(255, 247, 250, 244),
+        prefixIcon: Icon(Icons.lock, color: Color.fromARGB(255, 91, 142, 85)),
+        suffixIcon: GestureDetector(
+          onTap: () {
+            setState(() {
+              _isPasswordVisible = !_isPasswordVisible;
+            });
+          },
+          child: Icon(
+            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+            color: Colors.grey,
+          ),
+        ),
+      ),
       validator: (value) {
         if (value == null || value.isEmpty) {
           return 'Please enter a password';
@@ -85,7 +179,14 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget _buildRegisterButton() {
     return ElevatedButton(
       onPressed: () => _onRegisterPressed(context),
-      child: Text('Register'),
+      style: ElevatedButton.styleFrom(
+        primary: Color.fromARGB(255, 91, 142, 85),
+        padding: EdgeInsets.symmetric(horizontal: 132, vertical: 20),
+      ),
+      child: Text(
+        'REGISTER',
+        style: TextStyle(fontSize: 16, fontFamily: 'Sf'),
+      ),
     );
   }
 
@@ -111,12 +212,12 @@ class _RegisterPageState extends State<RegisterPage> {
     showDialog(
       context: context,
       builder: (ctx) => AlertDialog(
-        title: const Text('Error'),
+        title: Text('Error'),
         content: Text(message),
         actions: [
           TextButton(
             onPressed: () => Navigator.pop(ctx),
-            child: const Text('OK'),
+            child: Text('OK'),
           ),
         ],
       ),
